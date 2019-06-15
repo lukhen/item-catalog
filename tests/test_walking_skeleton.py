a8e3efabb08e6aff97d8949078cd1b301b94bf4a
lukhen <lukasz.hen@gmail.com>
2019-06-15 05:52:10 +0200

    The walking skeleton test is more automated, though still not fully.

--- tests/test_walking_skeleton.py
+++ tests/test_walking_skeleton.py
@@ -1,10 +1,21 @@
 import pytest
 import sys
 import requests
+import pathlib
+import subprocess
+import time
+
+
+@pytest.fixture
+def app_process():
+    install_app_from_github()
+    proc = subprocess.Popen(["./tmp/.venv/bin/python", "-m", "flaskapp"])
+    yield proc
+    proc.kill()
 
 
 @pytest.mark.e2e
-def test_e2e():
+def test_e2e(app_process):
     """
     This is the first teeny-tiny feature of the item-catalog app.
     I call this test the 'walking skeleton', after Nat Pryce in his book.
@@ -17,18 +28,19 @@ def test_e2e():
     A new-comming user enters the item-catalog web site http://localhost:5000.
     The page loads without any errors.
     """
+    time.sleep(1)
+    status_code = requests.get("http://localhost:5000").status_code
 
-    install_app_from_github()
-    run_app()
-
-    resp = requests.get("http://localhost:5000")
-
-    assert resp.status_code == 200
+    assert status_code == 200
 
 
 def install_app_from_github():
-    ...
-
-
-def run_app():
-    ...
+    pathlib.Path("./tmp").mkdir(parents=True, exist_ok=True)
+    subprocess.run(["python", "-m", "venv", "./tmp/.venv"])
+    subprocess.run(
+        [
+            "./tmp/.venv/bin/pip",
+            "install",
+            "git+https://github.com/lukhen/item-catalog.git",
+        ]
+    )
