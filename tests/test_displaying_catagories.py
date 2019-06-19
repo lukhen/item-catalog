1ef9a6890c558c3aae0ae23db43ccffe1fdb6327
lukhen <lukasz.hen@gmail.com>
2019-06-19 10:30:34 +0200

    Confirmed we can display an empty list of categories.

--- tests/test_displaying_catagories.py
+++ tests/test_displaying_catagories.py
@@ -1,6 +1,15 @@
+from flaskapp import index
+import pytest
+
+
+@pytest.mark.e2e
 def test_e2e():
     """
     Manual test.
     When user enters the item-catalog web site http://localhost:5000
     He sees the list of all available categories
     """
+
+
+def test_displaying_list_of_categories():
+    assert index() == ""
