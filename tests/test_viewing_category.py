b4c193af827912598511f276bad81d5182cce88b
lukhen <lukasz.hen@gmail.com>
2019-06-25 05:03:25 +0200

    Manual e2e test for viewing a category feature.

---
+++ tests/test_viewing_category.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+@pytest.mark.e2e
+def test_e2e():
+    """
+    Manual test.
+    User want te see all items of 'sailing' category.
+    He enters the site /categories/sailing
+    He can see all sailing items in a column.
+    """
