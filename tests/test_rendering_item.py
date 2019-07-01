38e5b963b5093d8d094674ab463f1bf9a2b16b1d
lukhen <lukasz.hen@gmail.com>
2019-07-01 18:11:42 +0200

    Now we can render the item.

---
+++ tests/test_rendering_item.py
@@ -0,0 +1,14 @@
+from flaskapp import Item
+from flask import render_template
+import flaskapp
+
+
+def test_one_item():
+    item = Item(
+        category="sailing",
+        name="mainsheet",
+        description="A rope to manipulate the mainsail.",
+    )
+    with flaskapp.app.app_context():
+        output = render_template(flaskapp.ITEM_TEMPLATE, item=item)
+        assert item.name in output and item.description in output
