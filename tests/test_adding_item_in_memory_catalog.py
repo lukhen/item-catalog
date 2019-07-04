9d881e52e86b40685b959420be5740513f69e6a4
lukhen <lukasz.hen@gmail.com>
2019-07-04 08:37:33 +0200

    Extracted contract for adding an item to catalog.

--- tests/test_adding_item_in_memory_catalog.py
+++ tests/test_adding_item_in_memory_catalog.py
@@ -1,31 +1,40 @@
 from flaskapp import InMemoryCatalog, Item, ItemException
 import pytest
+from abc import ABC, abstractmethod
 
 
-def test_item_not_exists():
-    name = "mainsheet"
-    category = "sailing"
-    catalog = InMemoryCatalog()
-    catalog.add_item(Item(name=name, category=category))
+class AddItemToCatalogContract(ABC):
+    def test_item_not_exists(self):
+        name = "mainsheet"
+        category = "sailing"
+        catalog = self.catalog_with([], [])
+        catalog.add_item(Item(name=name, category=category))
 
-    assert catalog.find_item(category, name) == Item(name=name, category=category)
-    assert category in catalog.all_categories()
+        assert catalog.find_item(category, name) == Item(name=name, category=category)
+        assert category in catalog.all_categories()
+
+    def test_item_not_exists_category_exists(self):
+        name = "mainsheet"
+        category = "sailing"
+        catalog = self.catalog_with([category], [])
+
+        catalog.add_item(Item(name=name, category=category))
+
+        assert len(catalog.all_categories()) == 1
+        assert catalog.find_item(category, name) == Item(name=name, category=category)
+
+    def test_item_exists(self):
+        item = Item(name="mainsail", category="sailing")
+        catalog = self.catalog_with(["sailing"], [item])
+        with pytest.raises(ItemException) as excinfo:
+            catalog.add_item(item)
+            assert "Item [{}] already exists.".format(item) in str(excinfo.value)
+
+    @abstractmethod
+    def catalog_with(categories, items):
+        ...
 
 
-def test_item_not_exists_category_exists():
-    name = "mainsheet"
-    category = "sailing"
-    catalog = InMemoryCatalog(categories=[category], items=[])
-
-    catalog.add_item(Item(name=name, category=category))
-
-    assert len(catalog.all_categories()) == 1
-    assert catalog.find_item(category, name) == Item(name=name, category=category)
-
-
-def test_item_exists():
-    item = Item(name="mainsail", category="sailing")
-    catalog = InMemoryCatalog(categories=["sailing"], items=[item])
-    with pytest.raises(ItemException) as excinfo:
-        catalog.add_item(item)
-        assert "Item [{}] already exists.".format(item) in str(excinfo.value)
+class TestAddItemToInMemoryCatalog(AddItemToCatalogContract):
+    def catalog_with(self, categories, items):
+        return InMemoryCatalog(categories=categories, items=items)
