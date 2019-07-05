8231a1ffc0efb0c7191f58f48d48db6cdce3c2d3
lukhen <lukasz.hen@gmail.com>
2019-07-05 15:37:59 +0200

    Gave a test method a more accurate name.

--- tests/test_retrieving_categories_from_catalog.py
+++ tests/test_retrieving_categories_from_catalog.py
@@ -1,6 +1,6 @@
 from flaskapp import InMemoryCatalog
 
 
-def test_retrieving_from_in_memory_catalog():
+def test_many_categories_in_memory_catalog():
     catalog = InMemoryCatalog(["Soccer", "Baseball", "Sailing"], [])
     assert catalog.all_categories() == ["Soccer", "Baseball", "Sailing"]
