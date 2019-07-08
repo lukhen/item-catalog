e1ff4c6dcfd5693d679b8896496deedc1cd39839
lukhen <lukasz.hen@gmail.com>
2019-07-08 12:43:06 +0200

    Extracted contract in a test.

--- tests/test_find_item_in_catalog.py
+++ tests/test_find_item_in_catalog.py
@@ -1,18 +1,28 @@
 from flaskapp import InMemoryCatalog, Item
+from abc import ABC, abstractmethod
 
 
-def test_item_exists_1():
-    item = Item(name="mainsheet", category="sailing")
-    catalog = InMemoryCatalog(categories=["sailing"], items=[item])
-    assert catalog.find_item("sailing", "mainsheet") == item
+class FindItemInCatalogContract(ABC):
+    @abstractmethod
+    def catalog_with(self, items):
+        ...
+
+    def test_item_exists_1(self):
+        item = Item(name="mainsheet", category="sailing")
+        catalog = self.catalog_with(categories=["sailing"], items=[item])
+        assert catalog.find_item("sailing", "mainsheet") == item
+
+    def test_item_exists_2(self):
+        items = [
+            Item(name="mainsheet", category="sailing"),
+            Item(name="ball", category="football"),
+        ]
+        catalog = self.catalog_with(categories=["sailing", "football"], items=items)
+        assert catalog.find_item("football", "ball") == Item(
+            name="ball", category="football"
+        )
 
 
-def test_item_exists_2():
-    items = [
-        Item(name="mainsheet", category="sailing"),
-        Item(name="ball", category="football"),
-    ]
-    catalog = InMemoryCatalog(categories=["sailing", "football"], items=items)
-    assert catalog.find_item("football", "ball") == Item(
-        name="ball", category="football"
-    )
+class TestFindItemInMemoryCatalog(FindItemInCatalogContract):
+    def catalog_with(self, categories, items):
+        return InMemoryCatalog(categories=categories, items=items)
